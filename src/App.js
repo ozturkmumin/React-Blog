d5861b4b45f6547b41e2ee3f541a3f6df33b0701
Mümin Öztürk <85707419+ozturkmumin@users.noreply.github.com>
2023-08-19 23:30:32 +0300

    Organizated navbar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import React from "react";
 function App() {
   return (
     <div>
-      <Navbar />
+      <Navbar/>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,22 @@
-function Navbar() {
-  return <div className="fw-bold text-danger">Navbar</div> 
-}
+import React from "react";
+import { Row, Col, Button } from "reactstrap";
 
-export default Navbar;
+export default class Example extends React.Component {
+  render() {
+    return (
+      <div className="p-4 navbar-shadow">
+        <Row>
+          <Col xs="4" lg="8">
+            <p className="h3"> Blog</p>
+          </Col>
+          <Col xs="3" lg="2" className="text-end">
+            <Button>Login</Button>
+          </Col>
+          <Col xs="auto" lg="2" className="text-start">
+            <Button color="primary">Sign Up</Button>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
